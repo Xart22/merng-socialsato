b65bb2e01837b48d0db43eaaa9a079cfff7e005c
Xart22 <fajarrizki2203@gmail.com>
2020-11-12 16:24:06 +0700

    hmm

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -9,7 +9,7 @@ function LikeButton({ user, post: { id, likeCount, likes } }) {
   const [liked, setLiked] = useState(false);
 
   const redi = () => {
-    window.location.href = "https://adoring-noyce-a29edf.netlify.app/login";
+    window.location.href = "https://social-sato.netlify.app/login";
   };
 
   useEffect(() => {
